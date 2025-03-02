a14371f708baefcc951df65c5c264d0613d44dac
satish chandra <imsachandra@gmail.com>
2025-03-03 04:02:38 +0530

    feat: Add comment and like models

---
+++ lec6/src/models/tweet.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
